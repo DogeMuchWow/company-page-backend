05e94bc02a65ea60cfeba096414844f96e07b449
DogeMuchWow <62595951+DogeMuchWow@users.noreply.github.com>
2024-07-02 09:43:36 +0700

    update file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { homeContentsModule } from './homeContents/homeContents.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+console.log(ConfigModule.forRoot());
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/companyDB')],
+  imports: [
+    //127 because node 18 and up prefer ipv6
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    homeContentsModule,
+  ],
   controllers: [],
   providers: [],
 })
