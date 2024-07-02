0e896d9d09c39318cf4435d07e8e1a5393adb39f
DogeMuchWow <62595951+DogeMuchWow@users.noreply.github.com>
2024-07-02 10:09:04 +0700

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { homeContentsModule } from './homeContents/homeContents.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-console.log(ConfigModule.forRoot());
 @Module({
   imports: [
     //127 because node 18 and up prefer ipv6
