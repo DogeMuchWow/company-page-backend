a4d3f93fbdb045d849e5c1db17b2acedd3fcfb12
DogeMuchWow <62595951+DogeMuchWow@users.noreply.github.com>
2024-07-02 09:46:22 +0700

    update main.ts with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    abortOnError: false,
+  });
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Company page')
+    .setDescription('Company page API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
