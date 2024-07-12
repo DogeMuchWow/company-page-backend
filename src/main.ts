1c8b22d0fd1cab07f9d136e2fd0d9e82f307c23c
DogeMuchWow <62595951+DogeMuchWow@users.noreply.github.com>
2024-07-12 10:19:20 +0700

    update configs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as express from 'express';
+import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     abortOnError: false,
   });
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Company page')
     .setDescription('Company page API')
@@ -14,7 +18,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.use('/images', express.static(join(__dirname, '..', 'uploads/images')));
   await app.listen(3000);
 }
 bootstrap();
