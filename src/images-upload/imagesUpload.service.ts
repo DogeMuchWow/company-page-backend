a2945109047ef8c72cab003b569af56e08e0b097
DogeMuchWow <62595951+DogeMuchWow@users.noreply.github.com>
2024-07-12 16:57:28 +0700

    update image vadiation

--- src/images-upload/imagesUpload.service.ts
+++ src/images-upload/imagesUpload.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { promises } from 'fs';
 import { extname } from 'path';
 import sharp from 'sharp';
@@ -10,28 +10,43 @@ export class ImagesUploadService {
     const imageUniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}${extname(image.originalname)}`;
     const imagePath = `./upload/images/${imageUniqueName}`;
     const readImage = await promises.readFile(image.path);
-
+    if (
+      !image.mimetype ||
+      !image.mimetype
+        .toString()
+        .toLowerCase()
+        .includes('png' || 'jpeg' || 'jpg' || 'jfif')
+    ) {
+      return '';
+    }
+    try {
+      await sharp(readImage).metadata();
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+      console.error('Image validation error', error);
+      return '';
+    }
     try {
       const imageUploadBuffer = await sharp(readImage)
         .resize({ fit: sharp.fit.inside })
+        .png({ quality: 70 })
         .jpeg({ quality: 70 })
         .toBuffer();
       await fs.mkdir('./upload/images', { recursive: true });
       await fs.writeFile(imagePath, imageUploadBuffer);
-      const testImage = sharp(imageUploadBuffer);
-      const metadata = await testImage.metadata();
-      console.log(metadata);
-      console.log(image);
-      console.log(readImage);
+      return `upload/images/${imageUniqueName}`;
     } catch (error) {
       console.error('ERROR: ' + error);
+      return '';
+    } finally {
+      try {
+        await fs.unlink(image.path);
+        console.log('Temp image remove');
+      } catch (error) {
+        console.error('ERROR:' + error);
+      }
     }
-    try {
-      await fs.unlink(image.path);
-      console.log('Temp image remove');
-    } catch (error) {
-      console.error('ERROR:' + error);
-    }
-    return `upload/images/${imageUniqueName}`;
   }
 }
