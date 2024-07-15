44696dca80a5048805407f2875844323528149da
DogeMuchWow <62595951+DogeMuchWow@users.noreply.github.com>
2024-07-15 11:14:03 +0700

    update image upload service

--- src/images-upload/imagesUpload.service.ts
+++ src/images-upload/imagesUpload.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { promises } from 'fs';
 import { extname } from 'path';
 import sharp from 'sharp';
@@ -10,43 +10,58 @@ export class ImagesUploadService {
     const imageUniqueName = `${Date.now()}-${Math.round(Math.random() * 1e9)}${extname(image.originalname)}`;
     const imagePath = `./upload/images/${imageUniqueName}`;
     const readImage = await promises.readFile(image.path);
-    if (
-      !image.mimetype ||
-      !image.mimetype
-        .toString()
-        .toLowerCase()
-        .includes('png' || 'jpeg' || 'jpg' || 'jfif')
-    ) {
-      return '';
-    }
     try {
-      await sharp(readImage).metadata();
-    } catch (error) {
-      if (error instanceof BadRequestException) {
-        throw error;
+      const allowFile = this.allowFileType(image);
+      if (allowFile) {
+        if (image.mimetype.toString().toLocaleLowerCase().includes('svg')) {
+          await fs.mkdir('./upload/images', { recursive: true });
+          await fs.writeFile(imagePath, readImage);
+        } else {
+          const imageUploadBuffer = await this.imageSharpBuffer(readImage);
+          await fs.mkdir('./upload/images', { recursive: true });
+          await fs.writeFile(imagePath, imageUploadBuffer);
+        }
+        return `upload/images/${imageUniqueName}`;
+      } else {
+        return '';
       }
-      console.error('Image validation error', error);
-      return '';
-    }
-    try {
-      const imageUploadBuffer = await sharp(readImage)
-        .resize({ fit: sharp.fit.inside })
-        .png({ quality: 70 })
-        .jpeg({ quality: 70 })
-        .toBuffer();
-      await fs.mkdir('./upload/images', { recursive: true });
-      await fs.writeFile(imagePath, imageUploadBuffer);
-      return `upload/images/${imageUniqueName}`;
     } catch (error) {
       console.error('ERROR: ' + error);
       return '';
     } finally {
       try {
         await fs.unlink(image.path);
-        console.log('Temp image remove');
+        console.log('Temp file remove');
       } catch (error) {
         console.error('ERROR:' + error);
       }
     }
+  }
+
+  allowFileType(file: Express.Multer.File) {
+    const mimeType = file.mimetype.toString().toLowerCase();
+    if (
+      file.mimetype &&
+      (mimeType.includes('png') ||
+        mimeType.includes('jpeg') ||
+        mimeType.includes('jpg') ||
+        mimeType.includes('jfif') ||
+        mimeType.includes('svg'))
+    ) {
+      return true;
+    } else {
+      console.error('File type not allowed');
+      return false;
+    }
+  }
+
+  async imageSharpBuffer(file: Buffer) {
+    await sharp(file).metadata();
+    const imageBuffer = await sharp(file)
+      .resize({ fit: sharp.fit.inside })
+      .png({ quality: 70 })
+      .jpeg({ quality: 70 })
+      .toBuffer();
+    return imageBuffer;
   }
 }
